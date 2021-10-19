3da62a4a32fde5957f1a3f3c5b60bad387c903c4
joehaozhang <joehaozhang@tencent.com>
2021-10-19 19:15:10 +0800

    feat: 模板修改为 ejs 语法

---
+++ magic-cli-dev-template-vue3/template/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+vue3 tempalte (version 1.0.1)
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%; 
+}
+</style>
